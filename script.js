791334534dfad992b797c5724f7eaf3d8a50902e
vidal james cubero <cubero.vj@gmail.com>
2022-03-17 01:02:24 +0800

    added to prod

--- script.js
+++ script.js
@@ -19,3 +19,25 @@ window.addEventListener('resize', function(){
         `
     }
 })
+
+window.onload = function() {
+    if (window.screen.width < 940 && window.screen.width > 600) {
+
+        let pageContainer=document.querySelector(".inspiration-on-demand-desktop")
+        let container = document.querySelector(".inspiration-on-demand-container")
+
+        let scale = window.screen.width / 940 ;
+        let height = scale * 7711 ;
+
+        document.querySelector(".inspiration-on-demand-desktop").style.cssText =`
+        transform: scale(${scale});
+        height: ${height}px;
+        transform-origin: left top;
+        -webkit-transform: scale(${scale});
+        -webkit-transform-origin: left top;
+        `
+    } else {
+        document.querySelector(".inspiration-on-demand-desktop").style.cssText =`
+        `
+    }
+}
